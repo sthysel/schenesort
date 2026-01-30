59137091f8c4817a029463855d13bc6d64af60b1
sthysel <sthysel@gmail.com>
2026-01-30 22:49:45 +0800

    tests

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Schenesort v0.0.1
+# Schenesort v0.0.2
 
 A CLI tool for managing wallpaper collections.
 
--- src/schenesort/__init__.py
+++ src/schenesort/__init__.py
@@ -1,3 +1,3 @@
 """Schenesort - Wallpaper collection management CLI tool."""
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
